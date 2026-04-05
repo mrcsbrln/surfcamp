f80d3a22750fd7bce030fcfaee95fa908eedab27
mrcsbln <hrtmnn.mrcs@gmail.com>
2026-04-05 16:53:22 +0200

    progress

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,21 +1,21 @@
+import { HeroSection } from "@/components/blocks/HeroSection";
+import { getHomePage } from "@/data/loaders";
+import { notFound } from "next/navigation";
+
 async function loader() {
-  const path = "/api/home-page";
-  const BASE_URL = "http://localhost:1337";
-  const url = new URL(path, BASE_URL);
-
-  const response = await fetch(url.href);
-  const data = await response.json();
+  const data = await getHomePage();
+  if (!data) notFound();
   console.log(data);
-
   return { ...data.data };
 }
 
 export default async function HomeRoute() {
   const data = await loader();
+  const blocks = data?.blocks || [];
+  console.log(data);
   return (
     <div>
-      <h1>{data.title}</h1>
-      <p>{data.description}</p>
+      <HeroSection {...blocks[0]} />
     </div>
   );
 }
